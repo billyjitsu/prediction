10cb3b2de011641dc82656c7ea14e2ff721e1d37
billyjitsu <82428978+billyjitsu@users.noreply.github.com>
2023-07-03 00:02:39 -0700

    update deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,19 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const proxyWBTC = "0x28Cac6604A8f2471E19c8863E8AfB163aB60186a";
+  const usdcAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"; // neecd to fix
+  const wbtcAddress = "0x1BFD67037B42Cf73acF2047067bd4F2C47D9BfD6"; // need to fix
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const predict = await ethers.deployContract("Prediction", [proxyWBTC, usdcAddress, wbtcAddress], );
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  await predict.waitForDeployment();
 
-  await lock.waitForDeployment();
+  console.log(`Predict contract deployed to ${predict.target}`);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // Wait for the transaction to be mined, and get the transaction receipt
+  const receipt = await predict.deploymentTransaction();
+  console.log(`Gas used: ${receipt?.gasPrice?.toString()}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
